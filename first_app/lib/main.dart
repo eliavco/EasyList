b1f7b320bdf58c65471e9ec68ff4f66ea94cbe32
Eliav Cohen <eliav.s.cohen@gmail.com>
2019-06-17 20:42:36 +0300

    17.6.2019 8:41 Section 5 start

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
         accentColor: blueColorCustom,
 
         // Fonts
-
+        fontFamily: 'Google'
 
       ),
 
--- first_app/lib/products.dart
+++ first_app/lib/products.dart
@@ -1,6 +1,16 @@
 // Basic Material App Package
 import 'package:flutter/material.dart';
 
+// Leading Image
+Widget leadingImage(imgUrl){
+  return Container(
+    child: ClipRRect(child: Image.asset(imgUrl),borderRadius: BorderRadius.circular(4.0),),
+    padding: EdgeInsets.all(8.0),
+    margin: EdgeInsets.only(bottom: 2.0),
+    decoration: BoxDecoration(boxShadow: [BoxShadow(color: Colors.black.withOpacity(.05),offset: Offset(7.0, 7.0),blurRadius: 5.0)]),
+  );
+}
+
 // List Genrator
 class Products extends StatelessWidget {
   
@@ -12,17 +22,27 @@ class Products extends StatelessWidget {
   @override
   Widget build(BuildContext context){
 
+    
+    // Colors Helper
+    List colorsList = [
+      Theme.of(context).primaryColor.withOpacity(.2),
+      Theme.of(context).accentColor.withOpacity(.2),
+    ];
+
     // Main List for every product
     return Column(children: products.map((el) => 
 
       // Indiviual Item
-      Card(
+      Container(child: Card(
+
+        // Backgrond Color
+        color: colorsList[products.indexOf(el)%2],
 
         // Column of Parts of the item
         child: Column(children: <Widget>[
 
           // Leading Image
-          Image.asset('assets/food.jpg'),
+          leadingImage('assets/food.jpg'),
           
           // Text Description
           Text(el),
@@ -30,9 +50,11 @@ class Products extends StatelessWidget {
         ],),
       ),
 
+      decoration: BoxDecoration(boxShadow: [BoxShadow(color: Colors.black.withOpacity(.05),offset: Offset(7.0, 7.0),blurRadius: 5.0)]),
+
     // Duplicating the cards for every product 
-    ).toList()
-    
+    )).toList()
+
     ,);
   }
 }
